578b8a704658d3a5f22bd8bd723fee8d2649fde7
meli-tortelli <melissa.tortelli@gmail.com>
2022-05-04 20:51:10 -0300

    added state for the widget of the feedback report

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,19 +1,76 @@
-import { CloseButton } from "./CloseButton";
+import { useState } from 'react';
+import { CloseButton } from './CloseButton';
 
-export function WidgetForm () {
+import bugImageUrl from '../assets/bug.svg';
+import ideaImageUrl from '../assets/idea.svg';
+import thoughtImageUrl from '../assets/thought.svg';
+
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto',
+    },
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada',
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de uma nuvem',
+    },
+  },
+};
+
+type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
   return (
     <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
       <header>
         <span className='text-xl leading-6'>Deixe seu feedback</span>
-        
+
         <CloseButton />
       </header>
-      
-      <p>Hello World</p>
+
+      {!feedbackType ? (
+        <div className='flex  py-8 gap-2 w-full'>
+          {Object.entries(feedbackTypes).map(([key, value]) => {
+            return (
+              <button
+                key={key}
+                onClick={() => setFeedbackType(key as FeedbackType)}
+                className='bg-zinc-800 rounded-lg py-5 w-24 flex flex-col flex-1 items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none'
+                type='button'>
+                <img src={value.image.source} alt={value.image.alt} />
+                <span>{value.title}</span>
+              </button>
+            );
+          })}
+        </div>
+      ) : (
+        <p>Hello World</p>
+      )}
 
       <footer className='text-xs text-neutral-400'>
-        <span>Feito com ♥ pela <a className='underline underline-offset-1' href='https://rocketseat.com.br'>Rocketseat</a></span>
+        <span>
+          Feito com ♥ pela{' '}
+          <a
+            className='underline underline-offset-1'
+            href='https://rocketseat.com.br'>
+            Rocketseat
+          </a>
+        </span>
       </footer>
     </div>
-  )
+  );
 }
